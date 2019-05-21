484d649cc4118213944945baafac6f94c8a96a20
IvanaDjikic <ivdjikic@gmail.com>
2019-05-21 07:22:46 +0200

    platypus change

--- src/optional/Platypus.java
+++ src/optional/Platypus.java
@@ -2,6 +2,10 @@ package optional;
 
 public class Platypus {
 	private String name;
+	
+	public Platypus (String name) {
+		this.name = name;
+	}
 
 	void sayHi(){
 		System.out.println("The platypus " + name + " is smarter than your average platypus.");
@@ -9,9 +13,9 @@ public class Platypus {
 	
 	public static void main(String[] args) {
 		//1. Make an instance of your new pet platypus
-		
+		Platypus plat = new Platypus("Bob");
 		//2. Call the sayHi method
-		
+		plat.sayHi();
 		//3. Create a constructor in the platypus class so that you can give your platypus a name.
 	}
 }
